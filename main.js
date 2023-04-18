ed03aa2d45ac6002f90083ff3491114294a44690
CoderGarlCoDiNghomelikeAPrO <121907247+CoderGarlCoDiNghomelikeAPrO@users.noreply.github.com>
2023-04-18 17:49:42 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,81 +1,81 @@
 
-var last_position_of_x, last_position_of_y;
-var width = screen.width;
+// Create canvas variable
 
-    canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext("2d");
-    
-    color = "black";
-    width_of_line = 2;
-
-    //Define "new_width" and "new_height" variables to store new screen width and height.
-new_width=screen.width-70;
-new_height=screen.height-300;
+//Set initial positions for ball and hole images.
 
 
-    /*If the screen size is less than 992 (means 992px), then change the width and height of 
-    the canvas with the new width and new height */
-    if(width<992)
-    {
-        document.getElementById("myCanavs").width=new_width;    
-        document.getElementById("myCanavs").width=new_height;
-    
-    }
-    
+block_image_width = 5;
+block_image_height = 5;
 
-    //Change 'mousedown' event to 'touchstart'
-    canvas.addEventListener("mousedown", my_mousedown);  
-    //Change the function to 'my_touchstart'  
-    function my_mousedown(e)
-    {
-        //Log my_toustart in console.
-        console.log("Current postition of x and y coordinates");
-        SVGSymbolElement.log("x = "+ current_postition_of_touch_x+"y ="+ current_postioton_of_touch_y)
+function load_img(){
+	// write code to Upload golf image on the canvas
+	new_image();
+}
 
+function new_image()
+{
+	// write code to Upload ball image on canvas
+}
 
-        //Addictonal Activity start
-       
-       
-        //Addictonal Activity ends
+window.addEventListener("keydown", my_keydown);
 
-        //Update the last_position_of_x_touch and last_position_of_y_touch here.
-       
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	/* Check the coordinates of the ball and hole images to finish the game. 
+	And id coordinates matches them remove ball image, 
+	display "GAME OVER!!!" 
+	and make canvas border 'red'. */
+	
+	else{
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	}
+	
+	function up()
+	{
+		// Write a code to move ball upward.
+	}
 
+	function down()
+	{
+		 // Write a code to move ball downward.
+	}
 
+	function left()
+	{
+		if(ball_x >5)
+		{
+			// Write a code to move ball left side.
+		}
+	}
 
-    }
-
-    //Replace the 'mousemove' event with 'touchmove'.
-    canvas.addEventListener("mousemove", my_mousemove);
-    //Replace the "my_mousemove()" function with "my_touchmove()" function.
-    function my_mousemove(e)
-    {
-        //Log the "my_touchMove" in the console.
-        console.log("my_touchMove");
-
-        /*Update the "current_position_of_touch_x" and "current_position_of_touch_y" 
-        with the touched points on canvas.*/
-        current_position_of_touch_x = 
-        current_position_of_touch_y = 
-
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width_of_line;
-    
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-        ctx.moveTo(last_position_of_x, last_position_of_y);
-    
-        console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
-        ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
-        ctx.stroke();
-    
-        last_position_of_x = current_position_of_touch_x; 
-        last_position_of_y = current_position_of_touch_y;
-    }
-
-    //Additional activity to clear area.
-
-    
+	function right()
+	{
+		if(ball_x <=1050)
+		{
+			// Write a code to move ball right side.
+		}
+	}
+	
+}
 
